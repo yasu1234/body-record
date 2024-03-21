a5aee64a8e4d79fe5da93d527c520597bce86dd6
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-03-21 23:55:55 +0900

    会員登録ボタンクリックで会員登録画面に遷移できるようにする

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '/src/components/Home.vue';
 import AccountInteroduction from '/src/components/AccountIntroduction.vue';
-
+import Signup from '/src/components/Signup.vue';
 
 const routes = [
   {
@@ -13,6 +13,11 @@ const routes = [
     path: '/accountInteroduction', 
     name: 'AccountInteroduction', 
     component: AccountInteroduction,
+  },
+  {
+    path: '/signup', 
+    name: 'Signup', 
+    component: Signup,
   },
 ];
 
