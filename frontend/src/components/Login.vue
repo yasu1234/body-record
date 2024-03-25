ffc339dbbba811cba5fcd4bd4347476f65f10816
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-03-26 07:52:04 +0900

    ログイン画面の作成

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,94 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
+const login = async () => {
+  try {
+    const res = await axios.post(import.meta.env.VITE_APP_API_BASE + '/api/v1/auth/sign_in', {
+      email: email.value,
+      password: password.value
+    })
+    console.log(res.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
+
+<template>
+    <h1 class="signUpTitle">ログイン</h1>
+    <div class="singUpInput">
+        <form class="form">
+            <div class="item">
+                <label class="itemLabel">メールアドレス</label>
+                <input id="email" type="email" v-model="email">
+            </div>
+            <div class="item">
+                <label class="itemLabel" for="password">パスワード</label>
+                <input id="password" type="password" v-model="password">
+            </div>
+            <div class="signUpTitle">
+                <button class="loginButton" @click="login">ログイン</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<style>
+.form{
+   width: 100%;
+   margin:0 auto;
+   padding: 50px;
+   box-sizing: border-box;
+}
+
+.signUpTitle {
+    padding-top: 40px;
+    text-align: center;
+}
+ 
+.item{
+    padding-top: 40px;
+    width: 50%;
+    margin: 0 auto;
+}
+
+.itemLabel{
+    display: block;
+    text-align: left;
+}
+
+.error-message{
+    width: 50%;
+    margin: 0 auto;
+}
+ 
+.error-message-text{
+    color: red;
+    font-size: 15px;
+}
+
+.form input[type="email"],
+.form input[type="password"] {
+     padding: 10px;
+     width: 100%;
+ }
+ 
+.form textarea{
+   padding: 10px;
+   height: 100px;
+   font-size: 14px;
+   width: 50%;
+ }
+ 
+.loginButton{
+    background: #ffa500;
+    color: white;
+    font-size:16px;
+    font-weight:bold;
+    padding: 10px 50px;
+}
+</style>
