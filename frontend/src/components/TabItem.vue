0357b3beadb6a7d01417418af28da5a4a91656f7
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-04-04 07:16:55 +0900

    メニューのタブの作成

---
+++ frontend/src/components/TabItem.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+    menuTitle: String,
+    menuId: Number,
+    currentId: Number
+})
+
+const modelValue = defineModel()
+const model = defineModel('currentId')
+const count = ref(0)
+
+</script>
+
+<template>
+    <div>
+        <button @click="handleClick" class="tab-button" :class="[props.currentId === props.menuId ? 'active' : '', 'tab']">
+            {{props.menuTitle}}
+        </button>
+        <div class="active-tab" v-if="props.currentId === props.menuId"></div>
+    </div>
+</template>
+
+<style>
+.tab {
+  border-radius: 2px 2px 0 0;
+  background: #fff;
+  color: #867d74;
+  line-height: 24px;
+}
+.tab-button {
+    margin-left: 8px;
+    margin-right: 8px;
+}
+.tab:hover {
+  color: #000000;
+}
+.active {
+    color: #867d74;
+}
+.active-tab {
+    height: 4px;
+    background: #ffa500;
+}
+</style>
