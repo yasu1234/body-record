f25959431add2cfd0d71bbe1b741a0f6c5b8cdf9
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-04-06 11:48:12 +0900

    選択した日付を表示できるようにイベントとバインディングの追加

--- frontend/src/components/DatePicker.vue
+++ frontend/src/components/DatePicker.vue
@@ -4,17 +4,28 @@ import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
 import { format } from 'date-fns';
 
+const props = defineProps({
+    date: String
+})
+
+const emit = defineEmits(['update:date']);
+
+function updateDate(newDate) {
+  emit('update:date', format(newDate, 'yyyy/M/d'));
+}
+
 </script>
 
 <template>
     <VueDatePicker
-    v-model="date"
     locale="ja"
+    v-model="props.date"
     week-start="0"
     format="yyyy/M/d"
     auto-apply
     :month-change-on-scroll="false"
     :enable-time-picker="false"
+    :date="date"
     @update:model-value="updateDate"/>
 </template>
 
