a58ca213cb4f78b5a6bf4f86b5f60e754a912685
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-04-13 08:47:41 +0900

    他のユーザーの記録を検索できるようにする

--- backend/app/controllers/api/v1/records_controller.rb
+++ backend/app/controllers/api/v1/records_controller.rb
@@ -25,6 +25,33 @@ class Api::V1::RecordsController < ApplicationController
         render json: { records: @records, totalCount: @totalCount }, status: 200
     end
 
+    def index
+        @totalCount = 0
+
+        if api_v1_user_signed_in?
+            @user = current_api_v1_user
+        else
+        end
+
+        @records = Record.where(open_flg: true)
+
+        if params[:keyword].present? 
+            @records = @records.where("memo LIKE ?", "%#{params[:keyword]}%")
+        end
+
+        if params[:startDate].present?
+            @records = @records.where("date >= ?", params[:startDate])
+        end
+
+        if params[:endDate].present?
+            @records = @records.where("date <= ?", params[:endDate])
+        end
+
+        @totalCount = @records.count
+
+        render json: { records: @records, totalCount: @totalCount }, status: 200
+    end
+
     def create
         @user = current_api_v1_user
         @record = Record.new(record_register_params)
