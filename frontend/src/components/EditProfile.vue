7b050f9cd7e6512d1e1cfc0f2dd68bddb17d032c
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-04-24 05:25:11 +0900

    プロフィール登録・更新APIを追加して登録・更新を行えるようにする

--- frontend/src/components/EditProfile.vue
+++ frontend/src/components/EditProfile.vue
@@ -1,41 +1,100 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router'
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
 import DropFile from './DropFile.vue'
 import Header from './Header.vue'
 
+const route = useRoute();
+
 const goalWeight = ref(null);
 const goalFatPercentage = ref(null);
-const files = ref([]);
+const profile = ref('');
+const file = ref(null);
 const userThumbnail = ref(null);
+const userId = ref(0);
 
 function onFileChange(event) {
-    files.value = [...event];
+    file.value = [...event];
 }
 
-const registerRecord = async () => {
+onMounted(() => {
+    getProfile();
+});
 
+const getProfile = async () => {
+  userId.value = route.params.id
+  const id = route.params.id
+  
+    try {
+        const res = await axios.get(import.meta.env.VITE_APP_API_BASE + `/api/v1/profiles/${id}`, {
+            headers: {
+                'access-token' : Cookies.get('accessToken'),
+                'client':Cookies.get('client'),
+                'uid': Cookies.get('uid')
+            }
+        })
+
+        if (res.data.profile.goal_weight !== null) {
+            goalWeight.value = res.data.profile.goal_weight
+        }
+
+        if (res.data.profile.goal_fat_percentage !== null) {
+            goalFatPercentage.value = res.data.goal_fat_percentage
+        }
+
+        if (res.data.profile.profile !== null) {
+            profile.value = res.data.profile.profile
+        }
+    } catch (error) {
+        console.log({ error })
+    }
+}
+
+const updateProfile = async () => {
+    try {
+        const formData = new FormData();
+        formData.append('profile', profile.value);
+        formData.append('goal_weight', goalWeight.value);
+        formData.append('goal_fat_percentage', goalFatPercentage.value);
+
+        if (file.value !== null) {
+            formData.append('image', file.value);
+        }
+
+        const res = await axios.post(import.meta.env.VITE_APP_API_BASE + `/api/v1/profiles`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'access-token' : Cookies.get('accessToken'),
+                'client':Cookies.get('client'),
+                'uid': Cookies.get('uid')
+            }
+        })
+        
+    } catch (error) {
+        console.log({ error })
+    }
 }
 </script>
 
 <template>
     <Header />
-    <form @submit.prevent="login">
+    <form @submit.prevent="updateProfile">
         <div class="profile-edit-content">
             <label for="goal-weight">目標体重:</label>
-            <input type="text" id="goal-weight">
+            <input type="text" id="goal-weight" v-model="goalWeight">
             <label for="goal-weight" class="unit-label">kg</label>
         </div>
         <div class="profile-edit-content">
             <label for="goal-fat-percentage">目標体脂肪率:</label>
-            <input type="text" id="goal-fat-percentage">
+            <input type="text" v-model="goalFatPercentage">
             <label for="goal-fat-percentage" class="unit-label">%</label>
         </div>
         <div class="profile-area">
             <label for="comments">紹介文</label>
-            <textarea id="profile"></textarea>
+            <textarea v-model="profile"></textarea>
         </div>
         <div class="profile-image-change-container">
             <label>プロフィール画像変更</label>
