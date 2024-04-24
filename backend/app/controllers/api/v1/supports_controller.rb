d94823cb17e722adf9a11b77bd9dfdb4b394d6ea
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-04-25 06:33:00 +0900

    応援登録・解除APIの作成

---
+++ backend/app/controllers/api/v1/supports_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::SupportsController < ApplicationController
+    before_action :set_user
+
+    def create
+        supporting = @user.support(@support_user)
+        if supporting.save
+            user_json = @support_user.as_json(
+                include: [
+                    { supportings: { include: %i[supportings supporters] } },
+                    { supporters: { include: %i[supportings supporters] } }
+                ]
+            )
+            includes_user = user_json["supporters"].any? { |supporter| supporter["id"] == @user.id }
+            render json: { user: user_json, isSupport: includes_user ,status: 200 }
+        else
+        end
+    end
+
+    def destroy
+        supporting = @user.removeSupport(@support_user)
+        if supporting.destroy
+            user_json = @support_user.as_json(
+                include: [
+                    { supportings: { include: %i[supportings supporters] } },
+                    { supporters: { include: %i[supportings supporters] } }
+                ]
+            )
+            includes_user = user_json["supporters"].any? { |supporter| supporter["id"] == @user.id }
+            render json: { user: user_json, isSupport: includes_user ,status: 200 }
+        else
+        end
+    end
+
+    private
+
+    def set_user
+        if api_v1_user_signed_in?
+            @user = current_api_v1_user
+        else
+        end
+        @support_user = User.find(params[:id])
+    end
+end
