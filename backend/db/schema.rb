9b254bd2f050fa14990fb08f1f9465ecb3ec0a6f
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-04-24 09:52:57 +0900

    supportテーブルの作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_23_030252) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_23_222851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_23_030252) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "supports", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "support_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["support_id"], name: "index_supports_on_support_id"
+    t.index ["user_id", "support_id"], name: "index_supports_on_user_id_and_support_id", unique: true
+    t.index ["user_id"], name: "index_supports_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -112,4 +122,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_23_030252) do
   add_foreign_key "bookmarks", "knowledges"
   add_foreign_key "bookmarks", "users"
   add_foreign_key "profiles", "users"
+  add_foreign_key "supports", "users"
+  add_foreign_key "supports", "users", column: "support_id"
 end
