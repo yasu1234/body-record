2bc7f78bb4fbba44ea26894d872d57785099b418
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-04-26 07:40:55 +0900

    記録のコメントを登録するAPIの追加

--- backend/app/controllers/api/v1/comments_controller.rb
+++ backend/app/controllers/api/v1/comments_controller.rb
@@ -17,6 +17,18 @@ class Api::V1::CommentsController < ApplicationController
     end
 
     def create_record_comment
+        @record = Record.where(id: record_comment_params[:record_id].to_i).first
+
+        @comment = @record.comments.build(record_comment_params)
+        @comment.user_id = @user.id
+
+        if @comment.save
+            render json: {
+                record: @record.as_json(include: [:comments])
+            }, status: 200
+        else
+            render json: { error: @comment.errors.full_messages}, status: 422
+        end
     end
 
     def destroy
@@ -27,7 +39,7 @@ class Api::V1::CommentsController < ApplicationController
     end
 
     def record_comment_params
-        params.permit(:user_id, :record_id, :comment)
+        params.permit(:record_id, :comment)
     end
 
     private 
