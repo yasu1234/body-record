106457e23e64d5d3c601398646ec9be18d9b53b2
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-01 09:43:01 +0900

    開発環境でのメールの設定の追加

---
+++ backend/app/mailers/contact_mailer.rb
@@ -0,0 +1,13 @@
+class ContactMailer < ApplicationMailer
+    def complete
+        mail(to: ENV['DEVELOPMENT_MAIL_TO'], subject: 'お問合せが届きました')  do |format|
+            format.text { render plain: email_body }
+        end
+    end
+
+    private
+    
+    def email_body
+        "お問合せが届いたので確認してください"
+    end
+end
