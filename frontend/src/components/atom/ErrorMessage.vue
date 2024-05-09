dd56d3ac53ad1078f14f02dcfd76f8c26b38fd61
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-10 08:29:40 +0900

    エラーメッセージ表示用のコンポネント追加

---
+++ frontend/src/components/atom/ErrorMessage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Message from 'primevue/message';
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+
+const prop = defineProps(['errorMessage'])
+</script>
+
+<template>
+    <Message severity="error" v-if="prop.errorMessage !== ''" class="error-message">{{ prop.errorMessage }}</Message>
+</template>
+
+<style scoped>
+.error-message {
+    white-space:pre-wrap; 
+    word-wrap:break-word;
+}
+</style>
