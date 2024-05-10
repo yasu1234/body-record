3e986762cb1910411ec6027c01584a7189beda2f
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-11 08:40:18 +0900

    エラーメッセージは閉じれないようにする

--- frontend/src/components/atom/ErrorMessage.vue
+++ frontend/src/components/atom/ErrorMessage.vue
@@ -3,10 +3,11 @@ import Message from 'primevue/message';
 import 'primevue/resources/themes/md-light-indigo/theme.css';
 
 const prop = defineProps(['errorMessage'])
+
 </script>
 
 <template>
-    <Message severity="error" v-if="prop.errorMessage !== ''" class="error-message">{{ prop.errorMessage }}</Message>
+    <Message severity="error" v-show="prop.errorMessage !== ''" class="error-message" :closable="false">{{ prop.errorMessage }}</Message>
 </template>
 
 <style scoped>
