a43edff5a32eed63a671eba3a0f92ddacb7f4101
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-12 22:12:34 +0900

    パスワード入力用のコンポネント作成

---
+++ frontend/src/components/atom/PasswordText.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+import Password from 'primevue/password';
+
+const passwordType = ref(0);
+
+const props = defineProps({
+    password: String,
+    passwordType: Number
+})
+
+const emit = defineEmits(['updatePassword']);
+
+onMounted(() => {
+    passwordType.value = props.passwordType
+});
+
+function updatePassword(currentText) {
+  emit('updatePassword', currentText, passwordType.value);
+}
+
+</script>
+
+<template>
+    <Password v-model="props.password" :feedback="false" @update:model-value="updatePassword" class="password-text" toggleMask />
+</template>
+
+<style scoped>
+.password-text {
+    display: block;
+}
+</style>
