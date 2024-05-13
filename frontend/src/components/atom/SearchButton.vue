7d6882a0698b40033c0aa0ac57ca4e34f833d7cf
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-14 07:57:27 +0900

    検索ボタンをコンポネント化する

---
+++ frontend/src/components/atom/SearchButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Button from "primevue/button";
+
+const emit = defineEmits(["searchButtonClick"]);
+
+const searchButtonClick = () => {
+  emit("searchButtonClick");
+};
+</script>
+
+<template>
+  <Button
+    label="検索"
+    icon="pi pi-search"
+    class="search-button"
+    @click="searchButtonClick"
+  />
+</template>
+
+<style scoped>
+.search-button {
+  background: #ffa500;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+}
+</style>
