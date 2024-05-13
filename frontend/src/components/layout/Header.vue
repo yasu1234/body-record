f20e97c18cb9fb577d5e8d10bd5f0f9a5c89248e
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-14 06:37:47 +0900

    ヘッダーのメニュードロップダウンのデザイン調整(hoverなど)

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -1,171 +1,190 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { useRouter, useRoute } from 'vue-router'
-import Cookies from 'js-cookie';
-import axios from 'axios';
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 const router = useRouter();
 
 const isLogin = ref(false);
 const userId = ref(0);
+const showDropdown = ref(false);
 
 onMounted(() => {
-    checkLogin();
+  checkLogin();
 });
 
 const menuList = ref([
   { id: 1, label: "マイページ", isLogin: true },
   { id: 2, label: "設定", isLogin: true },
   { id: 3, label: "ログアウト", isLogin: true },
-  { id: 4, label: "問い合わせ", isLogin: false }
+  { id: 4, label: "問い合わせ", isLogin: false },
 ]);
 
-const checkLogin = async () => {
-  try {
-    const res = await axios.get(import.meta.env.VITE_APP_API_BASE + '/api/v1/users/check_login', {
-      headers: {
-        'access-token' : Cookies.get('accessToken'),
-        'client':Cookies.get('client'),
-        'uid': Cookies.get('uid'),
-      },
-    })
-
-    isLogin.value = (res.data.user !== null)
-    userId.value = res.data.user.id
-  } catch (error) {
-    isLogin.value = false
-  }
-}
-
-const showDropdown = ref(false);
-
-function showMyPage() {
-    router.push({ name: 'UserProfile', params: { id: userId.value }})
-}
-
-function showAccountIntroduction() {
-    router.push({ name: 'AccountInteroduction'})
-}
-
-function showSetting() {
-    router.push({ name: 'Setting'})
-}
-
-function showContact() {
-    router.push({ name: 'Contact'})
-}
-
-function toggleDropdown() {
+const toggleDropdown = () => {
   showDropdown.value = !showDropdown.value;
 }
 
-function showMenu(menu) {
-    switch(menu.id) {
-        case 1:
-            showMyPage();
-            break
-        case 2:
-            showSetting();
-            break
-        case 3:
-            logout();
-            break
-        case 4:
-            showContact();
-            break
-        default:
-            break
-    }
-}
+const checkLogin = async () => {
+  try {
+    const res = await axios.get(
+      import.meta.env.VITE_APP_API_BASE + "/api/v1/users/check_login",
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+
+    isLogin.value = (res.data.user !== null);
+    userId.value = res.data.user.id;
+  } catch (error) {
+    isLogin.value = false;
+  }
+};
+
 
 const logout = async () => {
-    try {
-        const res = await axios.delete(import.meta.env.VITE_APP_API_BASE + '/api/v1/auth/sign_out', {
-            headers: {
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        Cookies.remove('accessToken')
-        Cookies.remove('client')
-        Cookies.remove('uid')
+  try {
+    const res = await axios.delete(
+      import.meta.env.VITE_APP_API_BASE + "/api/v1/auth/sign_out",
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    Cookies.remove("accessToken");
+    Cookies.remove("client");
+    Cookies.remove("uid");
 
-        showHomeThenRelaod()
-    } catch (error) {
-        console.log({ error })
-    }
+    showHomeThenRelaod();
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+const showMenu = (menu)=> {
+  switch (menu.id) {
+    case 1:
+      showMyPage();
+      break;
+    case 2:
+      showSetting();
+      break;
+    case 3:
+      logout();
+      break;
+    case 4:
+      showContact();
+      break;
+    default:
+      break;
+  }
+}
+
+const showMyPage = () => {
+  router.push({ name: "UserProfile", params: { id: userId.value } });
+}
+
+const showAccountIntroduction = () => {
+  router.push({ name: "AccountInteroduction" });
+}
+
+const showSetting = () => {
+  router.push({ name: "Setting" });
+}
+
+const showContact = () => {
+  router.push({ name: "Contact" });
 }
 
 const showHomeThenRelaod = async () => {
-  await router.push({ name: 'Home'})
-  router.go(0)
-}
+  await router.push({ name: "Home" });
+  router.go(0);
+};
 </script>
 
 <template>
-    <header>
-        <h2 class="title"><router-link to="/" class="title">In-Body.com</router-link></h2>
-        <nav>
-            <div class="navigation-buttons">
-                <button v-if="!isLogin" class="account-introducton-button" @click="showAccountIntroduction">会員登録・ログイン</button>
-                <div @click="toggleDropdown">
-                    <button class="user-button"><img src="../../assets/image/user-placeholder.png" alt="ユーザー" class="user-image"></button>
-                </div>                
-            </div>
-            <div v-if="showDropdown" class="dropdown-menu">
-                <button v-for="menu in menuList" class="menu-item" @click="showMenu(menu)">
-                    {{ menu.label }}
-                </button>
-            </div>
-        </nav>
-    </header>
+  <header>
+    <h2 class="title">
+      <router-link to="/" class="title">In-Body.com</router-link>
+    </h2>
+    <nav>
+      <div class="navigation-menus">
+        <button
+          v-if="!isLogin"
+          class="account-introducton-button"
+          @click="showAccountIntroduction"
+        >
+          会員登録・ログイン
+        </button>
+        <div @click="toggleDropdown">
+          <button class="user-button">
+            <img
+              src="../../assets/image/user-placeholder.png"
+              alt="ユーザー"
+              class="user-image"
+            />
+          </button>
+        </div>
+      </div>
+      <div v-if="showDropdown" class="dropdown-menu">
+        <button
+          v-for="menu in menuList"
+          class="menu-item"
+          @click="showMenu(menu)"
+        >
+          {{ menu.label }}
+        </button>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <style>
 header {
-    width: 100%;
-    height: 80px;
-    background: #f1f3ef;
-    padding-left: 30px;
-    padding-right: 30px;
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  width: 100%;
+  height: 80px;
+  background: #f1f3ef;
+  padding-left: 30px;
+  padding-right: 30px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 .title {
-    color:#ffa600;
+  color: #ffa600;
 }
-.navigation-buttons {
-    display: flex;
-    justify-content: space-between;
+.navigation-menus {
+  display: flex;
+  justify-content: space-between;
 }
 .account-introducton-button {
-    background: #ffa500;
-    margin-right: 30px;
+  background: #ffa500;
+  margin-right: 30px;
 }
 .user-button {
-    padding: 0;
-    background: transparent;
-    border: 1px solid #CCC;
-    border-radius: 50%;
+  padding: 0;
+  background: transparent;
+  border: 1px solid #ccc;
+  border-radius: 50%;
 }
 .user-image {
-    width: 40px;
-    height: 40px;
-}
-
-.user-dropdown {
-  position: relative;
-  display: inline-block;
+  width: 40px;
+  height: 40px;
 }
 
 .dropdown-menu {
   position: absolute;
   background-color: #f1f1f1;
   min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
   right: 5px;
   top: 80px;
@@ -175,5 +194,12 @@ header {
 }
 .menu-item {
   padding: 6px;
+  background: #ffffff;
+  color: #000000;
+  border-radius: 0px;
+}
+
+.menu-item:hover {
+  background-color: #eae1e1;
 }
 </style>
