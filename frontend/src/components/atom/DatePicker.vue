384355371196e50fb17c6ae921f2226799d133a8
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-17 00:33:38 +0900

    DatePickerで設定を解除したときにformatしないようにnullチェックを加える

--- frontend/src/components/atom/DatePicker.vue
+++ frontend/src/components/atom/DatePicker.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
 import { format } from 'date-fns';
+
+const date = ref('');
 
 const props = defineProps({
     date: String
@@ -10,16 +12,23 @@ const props = defineProps({
 
 const emit = defineEmits(['update:date']);
 
-function updateDate(newDate) {
-  emit('update:date', format(newDate, 'yyyy/M/d'));
-}
+onMounted(() => {
+  date.value = props.date
+});
 
+function updateDate(newDate) {
+  if (newDate !== null) {
+    emit('update:date', format(newDate, 'yyyy/M/d'));
+  } else {
+    emit('update:date', '');
+  }
+}
 </script>
 
 <template>
     <VueDatePicker
     locale="ja"
-    v-model="props.date"
+    v-model="date"
     week-start="0"
     format="yyyy/M/d"
     auto-apply
