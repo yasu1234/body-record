76833d5ba72eb2c7497fc82cc5ab8747447da119
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-18 11:06:33 +0900

    フォーマット調整

--- frontend/src/components/page/KnowledgeDetail.vue
+++ frontend/src/components/page/KnowledgeDetail.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue';
-import axios from 'axios';
-import { useRoute, useRouter } from 'vue-router'
-import Cookies from 'js-cookie';
-import MarkdownIt from 'markdown-it'
+import { ref, onMounted, computed } from "vue";
+import axios from "axios";
+import { useRoute, useRouter } from "vue-router";
+import Cookies from "js-cookie";
+import MarkdownIt from "markdown-it";
 
-import Header from '../layout/Header.vue'
+import Header from "../layout/Header.vue";
 
 const route = useRoute();
 const router = useRouter();
@@ -15,197 +15,240 @@ const imageUrls = ref([]);
 const knowledgeId = ref(null);
 const isBookmark = ref(false);
 const comments = ref([]);
-const comment = ref('');
+const comment = ref("");
 
-const md = new MarkdownIt()
+const md = new MarkdownIt();
 
 const renderedMarkdown = computed(() => {
-    if (knowledge.value && knowledge.value.content) {
-        return md.render(knowledge.value.content);
-    }
-    return '';
-})
+  if (knowledge.value && knowledge.value.content) {
+    return md.render(knowledge.value.content);
+  }
+  return "";
+});
 
 onMounted(() => {
-    getDetail();
+  getDetail();
 });
 
 const getDetail = async () => {
-    const id = route.params.id
-    try {
-        const res = await axios.get(import.meta.env.VITE_APP_API_BASE + `/api/v1/knowledges/${id}`, {
-            headers: {
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        knowledgeId.value = res.data.knowledge.id
-        knowledge.value = res.data.knowledge
-        imageUrls.value = res.data.knowledge.image_urls
-        isBookmark.value = res.data.isBookmark
-        comments.value = res.data.knowledge.comments
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
-        }
+  const id = route.params.id;
+  try {
+    const res = await axios.get(
+      import.meta.env.VITE_APP_API_BASE + `/api/v1/knowledges/${id}`,
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    knowledgeId.value = res.data.knowledge.id;
+    knowledge.value = res.data.knowledge;
+    imageUrls.value = res.data.knowledge.image_urls;
+    isBookmark.value = res.data.isBookmark;
+    comments.value = res.data.knowledge.comments;
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
     }
-}
+  }
+};
 
 const bookmarkOn = async () => {
-    try {
-        const formData = new FormData();
-        formData.append('bookmark[knowledge_id]', knowledgeId.value);
+  try {
+    const formData = new FormData();
+    formData.append("bookmark[knowledge_id]", knowledgeId.value);
 
-        const res = await axios.post(import.meta.env.VITE_APP_API_BASE + `/api/v1/bookmarks`, formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        knowledge.value = res.data.knowledge
-        isBookmark.value = res.data.knowledge.isBookmark
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
-        }
+    const res = await axios.post(
+      import.meta.env.VITE_APP_API_BASE + `/api/v1/bookmarks`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    knowledge.value = res.data.knowledge;
+    isBookmark.value = res.data.knowledge.isBookmark;
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
     }
-}
+  }
+};
 
 const bookmarkOff = async () => {
-    try {
-        const res = await axios.delete(import.meta.env.VITE_APP_API_BASE + `/api/v1/bookmarks/${knowledgeId.value}`, {
-            headers: {
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        knowledge.value = res.data.knowledge
-        isBookmark.value = res.data.knowledge.isBookmark
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
-        }
+  try {
+    const res = await axios.delete(
+      import.meta.env.VITE_APP_API_BASE +
+        `/api/v1/bookmarks/${knowledgeId.value}`,
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    knowledge.value = res.data.knowledge;
+    isBookmark.value = res.data.knowledge.isBookmark;
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
     }
-}
+  }
+};
 
 function bookmarkClick(isBookmarkOn) {
-    if (isBookmarkOn === true) {
-        bookmarkOff()
-    } else {
-        bookmarkOn();
-    }
+  if (isBookmarkOn === true) {
+    bookmarkOff();
+  } else {
+    bookmarkOn();
+  }
 }
 
 const addComment = async () => {
-    try {
-        const formData = new FormData();
-        formData.append('knowledge_id', knowledgeId.value);
-        formData.append('comment', comment.value);
+  try {
+    const formData = new FormData();
+    formData.append("knowledge_id", knowledgeId.value);
+    formData.append("comment", comment.value);
 
-        const res = await axios.post(import.meta.env.VITE_APP_API_BASE + `/api/v1/comments/knowledge`, formData, {
-            headers: {
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        comments.value = res.data.knowledge.comments
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
-        }
+    const res = await axios.post(
+      import.meta.env.VITE_APP_API_BASE + `/api/v1/comments/knowledge`,
+      formData,
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    comments.value = res.data.knowledge.comments;
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
     }
-}
+  }
+};
 
-const showNotFound = () =>  {
-    router.push({ name: 'NotFound'})
-}
+const showNotFound = () => {
+  router.push({ name: "NotFound" });
+};
 
 function edit() {
-    router.push({ name: 'EditKnowledge', params: { id: knowledgeId.value }})
+  router.push({ name: "EditKnowledge", params: { id: knowledgeId.value } });
 }
 </script>
 
 <template>
-    <Header />
-    <div class="wrap">
-		<div class="main">
-			<div class="main_content">
-                <div class="editor">
-                    <p id="title" class="knowledge-title" type="text" v-if="knowledge !== null"> {{ knowledge.title }} </p>
-                    <p class="knowledge-content" v-html="renderedMarkdown"></p>
-                    <div v-if="imageUrls.length!==0">
-                        <p class="inputTitle">関連画像</p>
-                        <div class="thumbnail-container">
-                            <div class="thumbnail" v-for="item in imageUrls">
-                                <div class="thumbnail-image">
-                                    <img :src="item.url" alt="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="relationImages">
-                            <button class="editButton" @click="edit">編集する</button>
-                        </div>
-                    </div>
+  <Header />
+  <div class="wrap">
+    <div class="main">
+      <div class="main_content">
+        <div class="editor">
+          <p
+            id="title"
+            class="knowledge-title"
+            type="text"
+            v-if="knowledge !== null"
+          >
+            {{ knowledge.title }}
+          </p>
+          <p class="knowledge-content" v-html="renderedMarkdown"></p>
+          <div v-if="imageUrls.length !== 0">
+            <p class="inputTitle">関連画像</p>
+            <div class="thumbnail-container">
+              <div class="thumbnail" v-for="item in imageUrls">
+                <div class="thumbnail-image">
+                  <img :src="item.url" alt="" />
                 </div>
-                <div class="comment-container">
-                    <div class="comment-container-title-area">
-                        <p class="comment-container-title">コメント</p>
-                    </div>
-                    <div v-for="comment in comments">
-                        <p> {{ comment.comment }} </p>
-                    </div>
-                    <textarea name="comment" rows="15" v-model="comment" class="comment-textarea" />
-                    <button class="registerButton" @click="addComment">コメントを投稿する</button>
-                </div>
-			</div>
-		</div>
-		<div class="side">
-			<div class="side_content">
-				<button v-if="isBookmark" class="booknmark-button"><img src="../../assets/image/bookmark_on.png" alt="ブックマーク" class="booknmark-image" @click="bookmarkClick(true)"></button>
-                <button v-else class="booknmark-button"><img src="../../assets/image/bookmark_off.png" alt="ブックマーク" class="booknmark-image" @click="bookmarkClick(false)"></button>
-			</div>
-		</div>
-	</div>
+              </div>
+            </div>
+            <div class="relationImages">
+              <button class="editButton" @click="edit">編集する</button>
+            </div>
+          </div>
+        </div>
+        <div class="comment-container">
+          <div class="comment-container-title-area">
+            <p class="comment-container-title">コメント</p>
+          </div>
+          <div v-for="comment in comments">
+            <p>{{ comment.comment }}</p>
+          </div>
+          <textarea
+            name="comment"
+            rows="15"
+            v-model="comment"
+            class="comment-textarea"
+          />
+          <button class="registerButton" @click="addComment">
+            コメントを投稿する
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="side">
+      <div class="side_content">
+        <button v-if="isBookmark" class="booknmark-button">
+          <img
+            src="../../assets/image/bookmark_on.png"
+            alt="ブックマーク"
+            class="booknmark-image"
+            @click="bookmarkClick(true)"
+          />
+        </button>
+        <button v-else class="booknmark-button">
+          <img
+            src="../../assets/image/bookmark_off.png"
+            alt="ブックマーク"
+            class="booknmark-image"
+            @click="bookmarkClick(false)"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .wrap {
-    display: grid;	
-    grid-template-columns: 4fr 1fr;
-    background-color: #f5f6f6;
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  background-color: #f5f6f6;
 }
 .side_content {
-	position: sticky;
-	top: 100px;
-    margin-left: 30px;
+  position: sticky;
+  top: 100px;
+  margin-left: 30px;
 }
 
 .main {
-    margin-left: 20px;
+  margin-left: 20px;
 }
 .booknmark-button {
-	padding: 0;
-    background: transparent;
-    border: 1px solid #CCC;
-    border-radius: 50%;
+  padding: 0;
+  background: transparent;
+  border: 1px solid #ccc;
+  border-radius: 50%;
 }
 .booknmark-image {
-    width: 40px;
-    height: 40px;
+  width: 40px;
+  height: 40px;
 }
 
-.editor{
-   padding: 30px;
-   border-radius: 8px;
-   background-color: #ffffff;
- }
+.editor {
+  padding: 30px;
+  border-radius: 8px;
+  background-color: #ffffff;
+}
 
- input[type=text] {
+input[type="text"] {
   width: 100%;
   padding: 12px 12px;
   margin: 8px 0;
@@ -214,65 +257,65 @@ function edit() {
   border-radius: 4px;
 }
 .knowledge-title {
-    font-weight: bold;
-    font-size: 22px;
+  font-weight: bold;
+  font-size: 22px;
 }
 .knowledge-content {
-    font-size: 22px;
-    padding-top: 20px;
+  font-size: 22px;
+  padding-top: 20px;
 }
 .relationImages {
-    padding: 20px;
+  padding: 20px;
 }
-.editButton{
-    font-size:16px;
-    font-weight:bold;
+.editButton {
+  font-size: 16px;
+  font-weight: bold;
 }
 .thumbnail-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  padding: 20px;
 }
 .thumbnail {
-    position: relative;
-    display: inline-block;
-    height: 200px;
-    margin-right: 15px;
-    margin-bottom: 15px;
-    padding-left: 20px;
+  position: relative;
+  display: inline-block;
+  height: 200px;
+  margin-right: 15px;
+  margin-bottom: 15px;
+  padding-left: 20px;
 }
 .thumbnail img {
-    height: 100%;
+  height: 100%;
 }
 .thumbnail-image {
-    height: 100%;
+  height: 100%;
 }
 .thumbnail-image img {
-    height: 100%;
+  height: 100%;
 }
 .thumbnail-actions {
-    position: absolute;
-    top: 5px;
-    right: 5px;
+  position: absolute;
+  top: 5px;
+  right: 5px;
 }
 .comment-container {
-    margin-top: 20px;
-    border-radius: 8px;
-    background-color: #ffffff;
+  margin-top: 20px;
+  border-radius: 8px;
+  background-color: #ffffff;
 }
 .comment-container-title-area {
-    border-bottom: 1px solid rgba(6, 6, 6, 0.17);
+  border-bottom: 1px solid rgba(6, 6, 6, 0.17);
 }
 
 .comment-textarea {
-    width: 100%;
-    margin-top: 20px;
+  width: 100%;
+  margin-top: 20px;
 }
 
 .comment-container-title {
-    margin-left: 20px;
-    padding-top: 20px;
-    font-weight: bold;
+  margin-left: 20px;
+  padding-top: 20px;
+  font-weight: bold;
 }
 </style>
