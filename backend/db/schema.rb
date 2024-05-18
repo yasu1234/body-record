2b31d62cf0cc3f2914428d6d9ea5d62985ec911d
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-18 17:50:14 +0900

    Userテーブルの未使用カラムの削除

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_05_074029) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_18_084447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -126,8 +126,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_05_074029) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "name"
-    t.string "nickname"
-    t.string "image"
     t.string "email"
     t.json "tokens"
     t.datetime "created_at", null: false
