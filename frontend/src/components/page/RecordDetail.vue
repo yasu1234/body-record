4bf2982b55a3f70b029559ebdb8d49f53b90e37a
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-19 18:50:56 +0900

    記録詳細画面のフォーマット修正

--- frontend/src/components/page/RecordDetail.vue
+++ frontend/src/components/page/RecordDetail.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue';
-import axios from 'axios';
-import { useRoute, useRouter } from 'vue-router'
-import Cookies from 'js-cookie';
-import MarkdownIt from 'markdown-it'
+import { ref, onMounted, computed } from "vue";
+import axios from "axios";
+import { useRoute, useRouter } from "vue-router";
+import Cookies from "js-cookie";
+import MarkdownIt from "markdown-it";
 
-import Header from '../layout/Header.vue'
-import ErrorMessage from '../atom/ErrorMessage.vue'
+import Header from "../layout/Header.vue";
+import ErrorMessage from "../atom/ErrorMessage.vue";
 import Comments from "../layout/Comments.vue";
 import CommentInput from "../layout/CommentInput.vue";
 
@@ -21,207 +21,225 @@ const recordUserId = ref(0);
 const isMyRecord = ref(false);
 const isSupport = ref(false);
 const comments = ref([]);
-const comment = ref('');
-const errorMessage = ref('');
+const comment = ref("");
+const errorMessage = ref("");
 
-const md = new MarkdownIt()
+const md = new MarkdownIt();
 
 const renderedMarkdown = computed(() => {
-    if (memo.value !== null && memo.value !== "") {
-        return md.render(memo.value)
-    }
-    return ""
-})
+  if (memo.value !== null && memo.value !== "") {
+    return md.render(memo.value);
+  }
+  return "";
+});
 
 onMounted(() => {
-    getDetail();
+  getDetail();
 });
 
 const getDetail = async () => {
-    const id = route.params.id
-    try {
-        const res = await axios.get(import.meta.env.VITE_APP_API_BASE + `/api/v1/records/${id}`, {
-            headers: {
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        recordId.value = res.data.record.id
-        memo.value = res.data.record.memo
-        isMyRecord.value = res.data.record.isMyRecord
-        imageUrls.value = res.data.record.image_urls
-        recordUserId.value = res.data.record.user_id
-        comments.value = res.data.record.comments
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
-        }
+  const id = route.params.id;
+  try {
+    const res = await axios.get(
+      import.meta.env.VITE_APP_API_BASE + `/api/v1/records/${id}`,
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    recordId.value = res.data.record.id;
+    memo.value = res.data.record.memo;
+    isMyRecord.value = res.data.record.isMyRecord;
+    imageUrls.value = res.data.record.image_urls;
+    recordUserId.value = res.data.record.user_id;
+    comments.value = res.data.record.comments;
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
     }
-}
+  }
+};
 
 const deleteRecord = async () => {
-    const id = recordId.value
-    try {
-        const res = await axios.delete(import.meta.env.VITE_APP_API_BASE + `/api/v1/records/${id}`, {
-            headers: {
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        router.push({ name: 'Home'})
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
-        } else {
-            errorMessage.value = ''
-            let errorMessages = '記録削除に失敗しました\n';
-            if (error.response.status === 422) {
-                if (Array.isArray(error.response.data.errors)) {
-                    errorMessages += error.response.data.errors.join('\n');
-                }
-            }
-            errorMessage.value = errorMessages
+  const id = recordId.value;
+  try {
+    const res = await axios.delete(
+      import.meta.env.VITE_APP_API_BASE + `/api/v1/records/${id}`,
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    router.push({ name: "Home" });
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
+    } else {
+      errorMessage.value = "";
+      let errorMessages = "記録削除に失敗しました\n";
+      if (error.response.status === 422) {
+        if (Array.isArray(error.response.data.errors)) {
+          errorMessages += error.response.data.errors.join("\n");
         }
+      }
+      errorMessage.value = errorMessages;
     }
-}
+  }
+};
 
 const supportOn = async () => {
-    try {
-        const formData = new FormData();
-        formData.append('id', recordUserId.value);
+  try {
+    const formData = new FormData();
+    formData.append("id", recordUserId.value);
 
-        const res = await axios.post(import.meta.env.VITE_APP_API_BASE + `/api/v1/supports`, formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        isSupport.value = res.data.isSupport;
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
+    const res = await axios.post(
+      import.meta.env.VITE_APP_API_BASE + `/api/v1/supports`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    isSupport.value = res.data.isSupport;
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
+    } else {
+      errorMessage.value = "";
+      let errorMessages = "応援に失敗しました\n";
+      if (error.response.status === 422) {
+        if (Array.isArray(error.response.data.errors)) {
+          errorMessages += error.response.data.errors.join("\n");
         } else {
-            errorMessage.value = ''
-            let errorMessages = '応援に失敗しました\n';
-            if (error.response.status === 422) {
-                if (Array.isArray(error.response.data.errors)) {
-                    errorMessages += error.response.data.errors.join('\n');
-                } else {
-                    errorMessages += error.response.data.errors;
-                }
-            }
-            errorMessage.value = errorMessages
+          errorMessages += error.response.data.errors;
         }
+      }
+      errorMessage.value = errorMessages;
     }
-}
+  }
+};
 
 const supportOff = async () => {
-    try {
-        const res = await axios.delete(import.meta.env.VITE_APP_API_BASE + `/api/v1/supports/${recordUserId.value}`, {
-            headers: {
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        isSupport.value = res.data.isSupport;
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
+  try {
+    const res = await axios.delete(
+      import.meta.env.VITE_APP_API_BASE +
+        `/api/v1/supports/${recordUserId.value}`,
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    isSupport.value = res.data.isSupport;
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
+    } else {
+      errorMessage.value = "";
+      let errorMessages = "応援解除に失敗しました\n";
+      if (error.response.status === 422) {
+        if (Array.isArray(error.response.data.errors)) {
+          errorMessages += error.response.data.errors.join("\n");
         } else {
-            errorMessage.value = ''
-            let errorMessages = '応援解除に失敗しました\n';
-            if (error.response.status === 422) {
-                if (Array.isArray(error.response.data.errors)) {
-                    errorMessages += error.response.data.errors.join('\n');
-                } else {
-                    errorMessages += error.response.data.errors;
-                }
-            }
-            errorMessage.value = errorMessages
+          errorMessages += error.response.data.errors;
         }
+      }
+      errorMessage.value = errorMessages;
     }
-}
+  }
+};
 
 function supportClick(isSupport) {
-    if (isSupport === true) {
-        supportOff()
-    } else {
-        supportOn();
-    }
+  if (isSupport === true) {
+    supportOff();
+  } else {
+    supportOn();
+  }
 }
 
 const addComment = async () => {
-    try {
-        const formData = new FormData();
-        formData.append('record_id', recordId.value);
-        formData.append('comment', comment.value);
+  try {
+    const formData = new FormData();
+    formData.append("record_id", recordId.value);
+    formData.append("comment", comment.value);
 
-        const res = await axios.post(import.meta.env.VITE_APP_API_BASE + `/api/v1/comments/record`, formData, {
-            headers: {
-                'access-token' : Cookies.get('accessToken'),
-                'client':Cookies.get('client'),
-                'uid': Cookies.get('uid')
-            }
-        })
-        comments.value = res.data.record.comments
-    } catch (error) {
-        if (error.response.status === 404) {
-            showNotFound()
-        }
+    const res = await axios.post(
+      import.meta.env.VITE_APP_API_BASE + `/api/v1/comments/record`,
+      formData,
+      {
+        headers: {
+          "access-token": Cookies.get("accessToken"),
+          client: Cookies.get("client"),
+          uid: Cookies.get("uid"),
+        },
+      }
+    );
+    comments.value = res.data.record.comments;
+  } catch (error) {
+    if (error.response.status === 404) {
+      showNotFound();
     }
-}
+  }
+};
 
-const showNotFound = () =>  {
-    router.push({ name: 'NotFound'})
-}
+const showNotFound = () => {
+  router.push({ name: "NotFound" });
+};
 
 const showEdit = () => {
-    router.push({ name: 'EditRecord', params: { id: recordId.value }})
-}
+  router.push({ name: "EditRecord", params: { id: recordId.value } });
+};
 </script>
 
 <template>
-    <Header />
-    <ErrorMessage :errorMessage="errorMessage"/>
-    <div class="wrap">
-		<div class="main">
-			<div class="main_content">
-                <div class="editor">
-                    <p id="title" class="knowledge-title" type="text"> {{ title }} </p>
-                    <p class="record-content" v-html="renderedMarkdown" />
-                </div>
-                <div v-if="imageUrls !== null && imageUrls.length!==0">
-                    <p class="inputTitle">関連画像</p>
-                    <div class="thumbnail-container">
-                        <div class="thumbnail" v-for="item in imageUrls">
-                            <div class="thumbnail-image">
-                                <img :src="item.url" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="radius-section">
-                     <div class="comment-container-title-area">
-                        <p class="comment-container-title">コメント</p>
-                    </div>
-                    <div v-if="comments.length !== 0">
-                        <Comments :comments="comments" />
-                    </div>
-                    <div v-else>
-                        <p>コメントはありません</p>
-                    </div>
-                    <CommentInput @addComment="addComment" />
-                </div>
-			</div>
-		</div>
-		<div class="side">
-			<div class="side_content">
-                <button v-if="isSupport" class="round-button">
+  <Header />
+  <ErrorMessage :errorMessage="errorMessage" />
+  <div class="wrap">
+    <div class="main">
+      <div class="main_content">
+        <div class="editor">
+          <p id="title" class="knowledge-title" type="text">{{ title }}</p>
+          <p class="record-content" v-html="renderedMarkdown" />
+        </div>
+        <div v-if="imageUrls !== null && imageUrls.length !== 0">
+          <p class="inputTitle">関連画像</p>
+          <div class="thumbnail-container">
+            <div class="thumbnail" v-for="item in imageUrls">
+              <div class="thumbnail-image">
+                <img :src="item.url" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="radius-section">
+          <div class="comment-container-title-area">
+            <p class="comment-container-title">コメント</p>
+          </div>
+          <div v-if="comments.length !== 0">
+            <Comments :comments="comments" />
+          </div>
+          <div v-else>
+            <p>コメントはありません</p>
+          </div>
+          <CommentInput @addComment="addComment" />
+        </div>
+      </div>
+    </div>
+    <div class="side">
+      <div class="side_content">
+        <button v-if="isSupport" class="round-button">
           <img
             src="../../assets/image/support_off.png"
             alt="応援"
@@ -253,9 +271,9 @@ const showEdit = () => {
             @click=""
           />
         </button>
-			</div>
-		</div>
-	</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
@@ -276,13 +294,13 @@ const showEdit = () => {
 .main {
   margin-left: 20px;
 }
-.editor{
-   padding: 30px;
-   border-radius: 8px;
-   background-color: #ffffff;
- }
+.editor {
+  padding: 30px;
+  border-radius: 8px;
+  background-color: #ffffff;
+}
 
- input[type=text] {
+input[type="text"] {
   width: 100%;
   padding: 12px 12px;
   margin: 8px 0;
@@ -291,12 +309,12 @@ const showEdit = () => {
   border-radius: 4px;
 }
 .knowledge-title {
-    font-weight: bold;
-    font-size: 22px;
+  font-weight: bold;
+  font-size: 22px;
 }
 .record-content {
-    font-size: 22px;
-    padding-top: 20px;
+  font-size: 22px;
+  padding-top: 20px;
 }
 .radius-section {
   margin-top: 20px;
@@ -304,27 +322,27 @@ const showEdit = () => {
   background-color: #ffffff;
 }
 .thumbnail-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  padding: 20px;
 }
 .thumbnail {
-    position: relative;
-    display: inline-block;
-    height: 200px;
-    margin-right: 15px;
-    margin-bottom: 15px;
-    padding-left: 20px;
+  position: relative;
+  display: inline-block;
+  height: 200px;
+  margin-right: 15px;
+  margin-bottom: 15px;
+  padding-left: 20px;
 }
 .thumbnail img {
-    height: 100%;
+  height: 100%;
 }
 .thumbnail-image {
-    height: 100%;
+  height: 100%;
 }
 .thumbnail-image img {
-    height: 100%;
+  height: 100%;
 }
 .round-button {
   padding: 0;
