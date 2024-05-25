bbbbce7bb93650a8a7159bd1d96a2e708a75ee73
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-26 07:55:44 +0900

    Headerのメニューをログイン・未ログインで切り替える

--- frontend/src/const/const.js
+++ frontend/src/const/const.js
@@ -2,5 +2,19 @@ export const PasswordType = {
     password: 1,
     passwordConfirm: 2,
     newPassword: 3,
-    newPasswordConfirm: 4,
+    newPasswordConfirm: 4
+}
+
+export const HeaderMenuList = [
+    { id: 1, label: "会員登録・ログイン", status: 2 },
+    { id: 2, label: "マイページ", status: 1 },
+    { id: 3, label: "設定", status: 1 },
+    { id: 4, label: "ログアウト", status: 1 },
+    { id: 5, label: "問い合わせ", status: 3 }
+];
+
+export const AccountStatusType = {
+    shouldLogin: 1,
+    unLogin: 2,
+    all: 3
 }
