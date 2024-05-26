fe6044a5d8c35ccdbf2d14a412b98d4c3f86615a
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-05-27 07:34:25 +0900

    タブを選択している場合には文字色を濃くする

--- frontend/src/components/atom/TabItem.vue
+++ frontend/src/components/atom/TabItem.vue
@@ -1,47 +1,49 @@
 <script setup>
-import { ref } from 'vue'
-
 const props = defineProps({
-    menuTitle: String,
-    menuId: Number,
-    currentId: Number
-})
+  menuTitle: String,
+  menuId: Number,
+  currentId: Number,
+});
 
-const emit = defineEmits(['menuTapped']);
+const emit = defineEmits(["menuTapped"]);
 
 function handleClick(menuId) {
-    emit('menuTapped', menuId);
+  emit("menuTapped", menuId);
 }
 </script>
 
 <template>
-    <div>
-        <button @click="handleClick(props.menuId)" class="tab-button" :class="[props.currentId === props.menuId ? 'active' : '', 'tab']">
-            {{props.menuTitle}}
-        </button>
-        <div class="active-tab" v-if="props.currentId === props.menuId"></div>
-    </div>
+  <div>
+    <button
+      @click="handleClick(props.menuId)"
+      class="tab-button"
+      :class="[props.currentId === props.menuId ? 'active' : '', 'tab']"
+    >
+      {{ props.menuTitle }}
+    </button>
+    <div class="active-tab" v-if="props.currentId === props.menuId"></div>
+  </div>
 </template>
 
 <style scoped>
 .tab {
-    background: #fff;
-    color: #867d74;
+  background: #fff;
+  color: #867d74;
 }
 .tab-button {
-    margin-left: 8px;
-    margin-right: 8px;
-    padding: 0.6em 1.2em;
+  margin-left: 8px;
+  margin-right: 8px;
+  padding: 0.6em 1.2em;
 }
 .tab:hover {
-    color: #000000;
+  color: #000000;
 }
 .active {
-    color: #867d74;
+  color: #000000;
 }
 .active-tab {
-    height: 4px;
-    background: #ffa500;
-    margin-top: 3px;
+  height: 4px;
+  background: #ffa500;
+  margin-top: 3px;
 }
 </style>
