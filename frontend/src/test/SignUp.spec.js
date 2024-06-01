c1abe26e485d7134139a9cb44b41b6c93e56dd2f
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-06-01 14:27:29 +0900

    会員登録画面のテストコード追加

--- frontend/src/test/SignUp.spec.js
+++ frontend/src/test/SignUp.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount, flushPromises } from '@vue/test-utils'
-import Login from '@/components/page/Login.vue'
+import axios from 'axios'
+import Signup from '@/components/page/Signup.vue'
 
 jest.mock('axios')
 jest.mock('vue-router', () => {
@@ -31,7 +32,7 @@ describe('SearchPage', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallowMount(Login, {
+    wrapper = shallowMount(Signup, {
         global: {
           stubs: ["router-link", "router-view"]
         }
@@ -48,29 +49,51 @@ describe('SearchPage', () => {
 
   it('バリデーションエラー(メールアドレス形式違い)', async () => {
     await wrapper.find('#email').setValue('invalid-email')
-    await wrapper.vm.handleSubmit()
+    await wrapper.vm.checkValidate()
     expect(wrapper.findAll('.validation-error-message')[0].text()).toBe('メールアドレスの形式で入力してください')
   })
 
   it('バリデーションエラー(メールアドレス入力なし)', async () => {
-    await wrapper.vm.handleSubmit()
+    await wrapper.vm.checkValidate()
     expect(wrapper.findAll('.validation-error-message')[0].text()).toBe('この項目は必須です')
   })
 
   it('バリデーションエラー(パスワード入力なし)', async () => {
-    await wrapper.vm.handleSubmit()
+    await wrapper.vm.checkValidate()
     expect(wrapper.findAll('.validation-error-message')[1].text()).toBe('この項目は必須です')
   })
 
   it('バリデーションエラー(パスワード文字数不足)', async () => {
     wrapper.vm.password = 'passw'
-    await wrapper.vm.handleSubmit()
+    await wrapper.vm.checkValidate()
     expect(wrapper.findAll('.validation-error-message')[1].text()).toBe('6文字以上で入力してください')
   })
 
   it('バリデーションエラー(パスワード文字数オーバー)', async () => {
     wrapper.vm.password = 'passwordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpassword'
-    await wrapper.vm.handleSubmit()
+    await wrapper.vm.checkValidate()
     expect(wrapper.findAll('.validation-error-message')[1].text()).toBe('128文字以下で入力してください')
+  })
+
+  it('バリデーションエラー(パスワード確認入力なし)', async () => {
+    await wrapper.vm.checkValidate()
+    expect(wrapper.findAll('.validation-error-message')[2].text()).toBe('この項目は必須です')
+  })
+
+  it('バリデーションエラー(パスワード確認文字数不足)', async () => {
+    wrapper.vm.passwordConfirm = 'passw'
+    await wrapper.vm.checkValidate()
+    expect(wrapper.findAll('.validation-error-message')[2].text()).toBe('6文字以上で入力してください')
+  })
+
+  it('バリデーションエラー(パスワード確認文字数オーバー)', async () => {
+    wrapper.vm.passwordConfirm = 'passwordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpassword'
+    await wrapper.vm.checkValidate()
+    expect(wrapper.findAll('.validation-error-message')[2].text()).toBe('128文字以下で入力してください')
+  })
+
+  it('バリデーションエラー(名前入力なし)', async () => {
+    await wrapper.vm.checkValidate()
+    expect(wrapper.findAll('.validation-error-message')[3].text()).toBe('この項目は必須です')
   })
 })
