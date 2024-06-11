2ec860371c6737e34c70707de0d0b774f2b8d857
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-06-11 19:19:20 +0900

    問い合わせ関連のCSSで不要なものが記載されていたので見直し

--- frontend/src/components/page/ContactDetail.vue
+++ frontend/src/components/page/ContactDetail.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { axiosInstance } from "../../const/axios.js";
+import { axiosInstance, setupInterceptors } from "../../const/axios.js";
+import { useToast } from "primevue/usetoast";
+import { toastService } from "../../const/toast.js";
+
+const toast = useToast();
+const toastNotifications = new toastService(toast);
 
 import Header from "../layout/Header.vue";
+import TabMenu from "../layout/TabMenu.vue";
 
 const route = useRoute();
 const router = useRouter();
@@ -21,6 +27,7 @@ const getDetail = async () => {
     const res = await axiosInstance.get(`/api/v1/contacts/${id}`);
     contact.value = res.data.contact;
   } catch (error) {
+    toastNotifications.displayError("問い合わせ内容の取得に失敗しました", "")
   }
 };
 
@@ -39,27 +46,38 @@ const statusChange = async (isComplete) => {
     );
     contact.value = res.data.contact;
   } catch (error) {
+    let errorMessages = "";
+    if (error.response != null && error.response.status === 422) {
+      if (Array.isArray(error.response.data.errors)) {
+        errorMessages += error.response.data.errors.join("\n");
+      } else {
+        errorMessages = error.response.data.errors;
+      }
+    }
+    toastNotifications.displayError("対応状況更新に失敗しました", errorMessages);
   }
 };
 </script>
 
 <template>
+  <Toast position="top-center" />
   <Header />
+  <TabMenu />
   <div v-if="contact !== null">
     <div class="p-7">
       <p id="title" class="knowledge-title" type="text">
         {{ contact.content }}
       </p>
     </div>
-    <div class="relationImages">
+    <div class="p-5">
       <button
-        class="editButton"
+        class="status-edit-button"
         v-if="contact.status === 1"
         @click="statusChange(true)"
       >
         対応中に戻す
       </button>
-      <button class="editButton" v-else @click="statusChange(false)">
+      <button class="status-edit-button" v-else @click="statusChange(false)">
         対応済にする
       </button>
     </div>
@@ -67,92 +85,12 @@ const statusChange = async (isComplete) => {
 </template>
 
 <style scoped>
-.wrap {
-  display: grid;
-  grid-template-columns: 4fr 1fr;
-}
-.side_content {
-  position: sticky;
-  top: 100px;
-}
-
-.booknmark-button {
-  padding: 0;
-  background: transparent;
-  border: 1px solid #ccc;
-  border-radius: 50%;
-}
-.booknmark-image {
-  width: 40px;
-  height: 40px;
-}
-input[type="text"] {
-  width: 100%;
-  padding: 12px 12px;
-  margin: 8px 0;
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
 .knowledge-title {
   font-weight: bold;
   font-size: 22px;
 }
-.knowledge-content {
-  font-size: 22px;
-  padding-top: 20px;
-}
-.relationImages {
-  padding: 20px;
-}
-.editButton {
-  background: #ffa500;
-  color: white;
+.status-edit-button {
   font-size: 16px;
   font-weight: bold;
-}
-.thumbnail-container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  padding: 20px;
-}
-.thumbnail {
-  position: relative;
-  display: inline-block;
-  height: 200px;
-  margin-right: 15px;
-  margin-bottom: 15px;
-  padding-left: 20px;
-}
-.thumbnail img {
-  height: 100%;
-}
-.thumbnail-image {
-  height: 100%;
-}
-.thumbnail-image img {
-  height: 100%;
-}
-.thumbnail-actions {
-  position: absolute;
-  top: 5px;
-  right: 5px;
-}
-.comment-container {
-  margin-top: 20px;
-  margin-left: 20px;
-}
-.comment-container-title-area {
-  border-bottom: 1px solid rgba(6, 6, 6, 0.17);
-}
-
-.comment-textarea {
-  width: 100%;
-  margin-top: 20px;
-}
-
-.comment-container-title {
-  text-align: left;
 }
 </style>
--- frontend/src/components/page/ContactList.vue
+++ frontend/src/components/page/ContactList.vue
@@ -4,6 +4,7 @@ import { useRouter } from "vue-router";
 import { axiosInstance } from "../../const/axios.js";
 
 import Header from "../layout/Header.vue";
+import TabMenu from "../layout/TabMenu.vue";
 
 const router = useRouter();
 
@@ -33,6 +34,7 @@ function clickContact(item) {
 
 <template>
   <Header />
+  <TabMenu />
   <div
     class="contact-card"
     v-for="item of searchResult"
@@ -42,53 +44,29 @@ function clickContact(item) {
     <div class="complete-image" v-if="item.status === 1">
       <p class="contact-complete">対応済</p>
     </div>
-    <div>
-      <p class="idea-content">{{ item.content }}</p>
+    <div class="pb-2.5">
+      <p class="mx-2.5 mt-2.5">{{ item.content }}</p>
     </div>
   </div>
 </template>
 
 <style scoped>
-.search-button-area {
-  text-align: center;
-}
-.search-button {
-  background: #ffa500;
-  color: white;
-  font-size: 16px;
-  font-weight: bold;
-}
-
 .contact-card {
   margin: 10px 12px 12px 12px;
   border: 1px solid #ccc;
   border-radius: 5px;
 }
-.idea-content {
-  margin: 10px 0px 10px 10px;
-}
-.add-button-area {
-  text-align: right;
-  margin-top: 20px;
-  padding-right: 40px;
-}
-.add-button {
-  background: #ffa500;
-  color: white;
-  font-size: 16px;
-  font-weight: bold;
-}
 .complete-image {
   border-radius: 20px;
   background: #ffa500;
   margin-top: 10px;
-  padding-right: 16px;
-  padding-left: 16px;
-  width: 50px;
+  padding: 0px 16px;
+  width: 100px;
   margin-left: 10px;
   font-weight: bold;
 }
 .contact-complete {
   font-size: 15px;
+  text-align: center;
 }
 </style>
