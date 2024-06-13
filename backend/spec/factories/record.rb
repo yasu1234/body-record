c9767a3529d4ae0c531b6b0d3ca7ba74e2247fe8
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-06-14 06:48:05 +0900

    記録のテストデータの日付をランダムにする

--- backend/spec/factories/record.rb
+++ backend/spec/factories/record.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :record do
-    date { "2024-05-03T00:00:00.000Z" }
+    sequence(:date) { |n| Date.today - rand(365) }
     weight { 65.1 }
     fat_percentage { 11.2 }
     memo { "サンプルメモ" }
