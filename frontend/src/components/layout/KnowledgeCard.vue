eb9e185e7dc28ef440ef337ae61cbdff22622aae
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-06-17 19:31:11 +0900

    記事のカードにブックマーク件数を表示させる

--- frontend/src/components/layout/KnowledgeCard.vue
+++ frontend/src/components/layout/KnowledgeCard.vue
@@ -4,10 +4,12 @@ import MarkdownIt from "markdown-it";
 
 const md = new MarkdownIt();
 
-const props = defineProps(['knowledge']);
+const props = defineProps(["knowledge"]);
 
 const knowledgeTitle = ref("");
 const knowledgeContent = ref("");
+const bookmarkCount = ref(0);
+const isMyBookmark = ref(false);
 
 onMounted(() => {
   setProps(props.knowledge);
@@ -24,13 +26,15 @@ const setProps = (knowledge) => {
   if (knowledge != null) {
     knowledgeTitle.value = knowledge.title;
     knowledgeContent.value = knowledge.content;
+    bookmarkCount.value = knowledge.bookmark_count;
+    isMyBookmark.value = knowledge.is_bookmark;
   }
 };
 
 const renderedMarkdown = computed(() => {
-  var slicedContent = ""
+  var slicedContent = "";
   if (knowledgeContent.value.length > 100) {
-    slicedContent = knowledgeContent.value.slice(0, 100) + '...';
+    slicedContent = knowledgeContent.value.slice(0, 100) + "...";
   } else {
     slicedContent = knowledgeContent.value;
   }
@@ -45,6 +49,23 @@ const renderedMarkdown = computed(() => {
     </h4>
     <div>
       <p class="knowledge-content" v-html="renderedMarkdown" />
+    </div>
+    <div class="bookmark-container mt-5 ml-5 pb-5">
+      <div v-if="isMyBookmark" class="round-bookamark">
+        <img
+          src="../../assets/image/bookmark_on.png"
+          alt="サポート中"
+          class="bookmark-image"
+        />
+      </div>
+      <div v-else class="round-bookamark">
+        <img
+          src="../../assets/image/bookmark_off.png"
+          alt="未サポート"
+          class="bookmark-image"
+        />
+      </div>
+      <p class="ml-2.5">{{ bookmarkCount }}</p>
     </div>
   </div>
 </template>
@@ -73,6 +94,24 @@ const renderedMarkdown = computed(() => {
 }
 .knowledge-content {
   margin: 10px 0px 10px 10px;
+}
+.bookmark-container {
+  display: flex;
+  align-items: center;
+}
+.round-bookamark {
+  width: 50px;
+  height: 50px;
+  background: transparent;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.bookmark-image {
+  width: 100%;
+  height: auto;
 }
 
 @media screen and (max-width: 768px) {
