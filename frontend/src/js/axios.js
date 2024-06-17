e8fa55b76292842638a8cf977ae78fbcf480233f
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-06-17 19:33:43 +0900

    CSRF対策で固有のヘッダーが含まれているかを判定する

--- frontend/src/js/axios.js
+++ frontend/src/js/axios.js
@@ -6,7 +6,8 @@ const baseURL = import.meta.env.VITE_APP_API_BASE;
 const axiosInstance = axios.create({
   baseURL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
   },
 });
 
@@ -14,6 +15,8 @@ axiosInstance.interceptors.request.use((config) => {
   config.headers["access-token"] = Cookies.get("accessToken");
   config.headers["client"] = Cookies.get("client");
   config.headers["uid"] = Cookies.get("uid");
+  // config.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+  // config.headers['X-Requested-With'] = 'XMLHttpRequest';
 
   return config;
 });
@@ -23,7 +26,7 @@ const setupInterceptors = (router) => {
     response => response,
     error => {
       // 共通のエラー処理
-      if (error.response.status === 404) {
+      if (error.response != null && error.response.status === 404) {
         router.push({ name: 'NotFound' })
       }
 
