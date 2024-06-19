029342a70a2567dcbfef66c014939015ec1d18b7
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-06-20 06:12:26 +0900

    セキュリティグループのtfファイル追加

---
+++ terraform/security_group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "security-group-rds" {
+  description = "RDS-SG"
+  name                   = "RDS-SG"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = {}
+  tags_all               = {}
+  vpc_id                 = "vpc-0a5d8dd2f75850a61"
+}
+
+resource "aws_security_group" "security-group-ecs" {
+  description = "ECS-SG"
+  name                   = "ECS-SG"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = {}
+  tags_all               = {}
+  vpc_id                 = "vpc-0a5d8dd2f75850a61"
+}
