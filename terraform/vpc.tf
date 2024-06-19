391cd4b25d04ce235aba753a1d69b1b3a7ac8f56
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-06-20 05:35:16 +0900

    VPCのtfファイル追加

---
+++ terraform/vpc.tf
@@ -0,0 +1,22 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform
+resource "aws_vpc" "vpc" {
+  assign_generated_ipv6_cidr_block     = false
+  cidr_block                           = "10.0.0.0/16"
+  enable_dns_hostnames                 = true
+  enable_dns_support                   = true
+  enable_network_address_usage_metrics = false
+  instance_tenancy                     = "default"
+  ipv4_ipam_pool_id                    = null
+  ipv4_netmask_length                  = null
+  ipv6_cidr_block                      = null
+  ipv6_cidr_block_network_border_group = null
+  tags = {
+    Name = "プロジェクト-vpc"
+  }
+  tags_all = {
+    Name = "プロジェクト-vpc"
+  }
+}
