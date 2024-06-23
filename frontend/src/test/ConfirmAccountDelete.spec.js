ba4e167aac75c5fb7ca25090f0d37cf062e68e8e
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2024-06-24 05:13:21 +0900

    スナップショットテストの追加

---
+++ frontend/src/test/ConfirmAccountDelete.spec.js
@@ -0,0 +1,64 @@
+import { shallowMount } from "@vue/test-utils";
+import ConfirmAccountDelete from "@/components/page/account/ConfirmAccountDelete.vue";
+
+jest.mock("axios");
+jest.mock("vue-router", () => {
+  const router = {
+    push: jest.fn(),
+    pathname: "/",
+    route: "",
+    asPath: "",
+    query: {},
+    events: {
+      on: jest.fn(),
+      off: jest.fn(),
+      emit: jest.fn(),
+    },
+  };
+  return {
+    useRouter: jest.fn().mockReturnValue(router),
+    useRoute: jest.fn().mockReturnValue({
+      params: {
+        id: 1,
+      },
+    }),
+  };
+});
+
+jest.mock("primevue/usetoast", () => ({
+  useToast: jest.fn(() => ({
+    add: jest.fn(),
+    removeAll: jest.fn(),
+  })),
+}));
+
+jest.mock("@/js/axios.js", () => ({
+  axiosInstance: {
+    get: jest.fn(),
+    interceptors: {
+      request: { use: jest.fn() },
+      response: { use: jest.fn() },
+    },
+  },
+  setupInterceptors: jest.fn(),
+}));
+
+describe("Contact", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(ConfirmAccountDelete, {
+      global: {
+        stubs: ["router-link", "router-view"],
+      },
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("SnapShotテスト初期表示", () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
